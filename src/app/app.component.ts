88c89a5e3602010fe1c94eac9884ddb69f4e15f6
Khizar Murad <123hashinclude@gmail.com>
2021-04-17 23:58:15 +0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class AppComponent {
   title = 'pwawithangular';
-
+  breakpoint: number;
   value = 'from business card demo';
   //public myAngularxQrCode: string = 'www.google.com';
   constructor(public dialog: MatDialog) {}
@@ -18,6 +18,30 @@ export class AppComponent {
       data: {
       }
     });
+  }
+  ngOnInit(): void {
+    if (window.innerWidth <= 700) {
+      this.breakpoint = 2
+    }
+    else if (window.innerWidth <= 1100) {
+      this.breakpoint = 3
+    }
+    else {
+      this.breakpoint = 3
+    }
+/*this.breakpoint = (window.innerWidth <= 400) ? 1 : 3;*/
+  }
+  onResize(event) {
+    if (event.target.innerWidth <= 700) {
+      this.breakpoint = 2
+    }
+    else if (event.target.innerWidth <= 1100) {
+      this.breakpoint = 3
+    }
+    else {
+      this.breakpoint = 3
+    }
+    //this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 3;
   }
 }
 @Component({
